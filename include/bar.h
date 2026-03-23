d1dee44f6e21b18ac59988137b5a982ddc48f88f
Emin M <idle.cycles26@gmail.com>
2026-03-22 20:36:42 -0400

    create bar.h

---
+++ include/bar.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+namespace backtester {
+
+struct Bar {
+    std::string date;
+    double close;
+    double high;
+    double low;
+    double open;
+    long volume;
+    };
+}
